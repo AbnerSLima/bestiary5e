731fa680db719134de3fbfa08484ef8c7eb6caf6
AbnerSLima <abnerdslima@gmail.com>
2024-06-06 10:21:30 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import { Topo } from './components/Topo'
+import { Cards } from './components/Cards'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <div>
+      <Topo />
+      <Cards />
+      </div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Cards.jsx
@@ -0,0 +1,29 @@
+import './Cards.css'
+
+export function Cards(props) {
+  return (
+    <div className="listCard">
+      <div className="listImg">
+          <img src="./icon_bestiary.png" alt="imagem do monstro" />
+      </div>
+      <div className="listData">
+        <h2
+          id="listName">
+          DRAGÃO VERMELHO ANCIÃO
+        </h2>
+        <span
+          id="listNd">
+            ND: 24
+        </span>
+        <span
+          id="listType">
+          Dragão
+        </span>
+        <span
+          id="sizeList">
+          Imenso
+        </span>
+      </div>
+    </div> 
+  )
+}
